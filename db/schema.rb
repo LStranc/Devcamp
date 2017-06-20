a6623c19d522b620c7368272bf1f66ddd8db3185
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:27:04 +0000

    Implemented show functionality for portfolio module

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20170615170517) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
-    t.string   "tittle"
+    t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
