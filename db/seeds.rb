a7b162109060e7babaa602c434fe3843c9f32bca
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 15:02:23 +0000

    Integrated friendly routes for blog posts and fixed spelling error in Schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do |blog|
 Blog.create!(  
-    tittle: "My Blog Post #{blog}",
+    title: "My Blog Post #{blog}",
     body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
     eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
     voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
