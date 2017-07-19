c5ce2a88fc03090a2f257e08d942dbe1cd610a89
Logan Stranc <jordan@devcamp.com>
2017-07-19 17:01:52 +0000

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,5 +59,18 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+  
+  def alerts
+    alert = (flash[:alerts] || flash[:error] || flash[:notice])
+  
+    if alert
+     alert_generator alert
+    end
+  end
+  
+  def alert_generator msg
+    js add_gritter(msg, title: "Logan Stranc Portfolio", time: 3000)
+  end
+    
 
 end
